5bcba7bd3b16af1bdbde9cdcbe570629509919b4
DecaMeron23 <e.meroni1@studenti.unibg.it>
2024-11-29 21:20:33 +0100

    Refactoring

---
+++ Altri Script/script_segnale_randomico_a_blocchi.m
@@ -0,0 +1,10 @@
+pw_3 = [];
+while length(pw_3) <= N
+
+    max_s = 5;
+    min_s = 0.5;
+    elementi = rand(1 , 1) * (max_s/Ts  - min_s/Ts)  + min_s/Ts ;
+    pw_3 = [pw_3 ; rand(1 , 1) * ones(floor(elementi) , 1) * (pw_max  - pw_min)  + pw_min];
+end
+
+pw_3 = pw_3(1:N);
